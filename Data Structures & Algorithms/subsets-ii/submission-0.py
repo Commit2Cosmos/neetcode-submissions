8dcfa2c0741a5554069d941c12db08cec2fcb876
Anton Belov <72341499+Commit2Cosmos@users.noreply.github.com>
2026-09-25 12:11:43 +0100

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        subsets = set()
+
+        def recur(i, curr_set):
+            subsets.add(tuple(curr_set))
+
+            for j in range(i, len(nums)):
+                curr_set.append(nums[j])
+                print(curr_set)
+                recur(j+1, curr_set)
+                curr_set.remove(nums[j])
+
+        recur(0, [])
+
+        return list(subsets)
